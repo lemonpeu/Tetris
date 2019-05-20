7c65ce123b8b45d5e386cd72a2447159c3376b2a
Paula Illanes <illanesrpaula@gmail.com>
2019-05-19 22:49:56 -0300

    falta

---
+++ script.js
@@ -0,0 +1,118 @@
+const canvas = document.getElementById("tetris");
+const context = canvas.getContext("2d");
+
+context.scale(20, 20);
+
+
+const matrix = [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+];
+
+function collide(arena, player) {
+    const [m, o] = [player.matrix, player.pos];
+    for (let y = 0; y < m.length; ++y) {
+        for (let x = 0; x < m[y].length; ++x) {
+            if (m[y][x] !== 0 &&
+                (arena[y + o.y] &&
+                arena[y + o.y][x + o.x]) !== 0) {
+                    return true;
+            }
+        }
+    }
+    return false;
+} 
+
+function createMatrix (w, h) {
+    const matrix = [];
+    while (h--) {
+        matrix.push(new Array(w).fill(0));
+    }
+    return matrix;
+}
+
+ function draw () {
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(player.matrix, player.pos);
+} 
+
+function drawMatrix(matrix, offset) {
+    matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                context.fillStyle = 'red';
+                context.fillRect(x + offset.x,
+                                 y + offset.y,
+                                  1, 1);
+            }
+        });
+    });
+}
+
+// Copia todos los valores del jugador (player) a la 'arena'
+// merge(arena, player);
+// console.table(arena);
+// Se va a ver una T (matrix) en la consola.
+
+function merge(arena, player) {
+    player.matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                arena[y + player.pos.y] [x + player.pos.x] = value;
+            }
+        });
+    });
+}
+
+function playerDrop() {
+    player.pos.y++;
+    // Cuando se toca el piso se comienza de nuevo
+    if (collide(arena, player)) {
+        player.pos.y--;
+        merge(arena, player);
+        player.pos.y = 0;
+    }
+    dropCounter = 0;
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+
+
+let lastTime = 0;
+function update(time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    dropCounter += deltaTime;
+    
+    if (dropCounter > dropInterval) {
+        player.pos.y++;
+        dropCounter = 0;
+    }
+
+    draw();
+    requestAnimationFrame(update);
+}
+
+const arena = createMatrix(12, 20);
+console.log(arena); console.table(arena);
+
+const player = {
+    pos: {x: 5, y: 5},
+    matrix: matrix,
+}
+
+document.addEventListener('keydown', event => {
+    if (event.keyCode === 37) {
+        player.pos.x--;
+    } else if (event.keyCode === 39) {
+        player.pos.x++;
+    } else if (event.keyCode === 40) {
+        playerDrop();
+    }
+});
+
+update();
+
